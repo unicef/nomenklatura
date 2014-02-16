75dac5966b41f2a688df3f617995aaf63bd76f23
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-16 19:48:42 +0100

    Remove all nomenklatura internals to be replaced by grano.

--- nomenklatura/model/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from nomenklatura.model.dataset import Dataset
-from nomenklatura.model.entity import Entity
-from nomenklatura.model.account import Account
-from nomenklatura.model.upload import Upload
