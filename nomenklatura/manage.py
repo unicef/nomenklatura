b0e31611ba7af204cee8f9ab67b51d0b420e43ad
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-16 19:48:59 +0100

    Restore the basic angular application runner.

--- nomenklatura/manage.py
+++ nomenklatura/manage.py
@@ -1,37 +1,5 @@
-from flask.ext.script import Manager
+from grano.manage import manager
+from nomenklatura.web import app
 
-from nomenklatura.core import app, db
-from nomenklatura.model import *
-from nomenklatura import views
-
-manager = Manager(app)
-
-
-@manager.command
-def createdb():
-    """ Make the database. """
-    db.create_all()
-
-
-@manager.command
-def postproc_20131119():
-    from nomenklatura.model.text import normalize_text
-    for entity in Entity.query:
-        print [entity]
-        entity.normalized = normalize_text(entity.name)
-        #entity.attributes = entity.data
-        db.session.add(entity)
-        db.session.commit()
-
-
-@manager.command
-def flush(dataset):
-    ds = Dataset.by_name(dataset)
-    for alias in Alias.all_unmatched(ds):
-        db.session.delete(alias)
-    db.session.commit()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
-
--- nomenklatura/static/js/services/session.js
+++ nomenklatura/static/js/services/session.js
@@ -1,11 +1,11 @@
 nomenklatura.factory('session', ['$http', function($http) {
     var _authz = {},
-        dfd = $http.get('/api/2/sessions');
+        dfd = $http.get('/api/1/sessions');
 
     var requestAuthz = function(dataset_name) {
         if (!(dataset_name in _authz)) {
             _authz[dataset_name] = $http({
-                url: '/api/2/sessions/authz',
+                url: '/api/1/sessions/authz',
                 method: 'GET',
                 params: {dataset: dataset_name}});
         }
