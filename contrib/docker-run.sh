6deeef0b02254314db855d7f522b19e1f8829c94
Maximilian Richt <maxi@richt.name>
2015-12-12 11:14:18 +0100

    Add FORWARDED_ALLOW_IPS environment variable to enable https redirects behind a reverse proxy

--- contrib/docker-run.sh
+++ contrib/docker-run.sh
@@ -4,9 +4,14 @@ mkdir -p nomenklatura/static/vendor
 bower install --allow-root --config.interactive=false
 python nomenklatura/manage.py createdb
 # python nomenklatura/manage.py assets build
+
+# sets FORWARDED_ALLOW_IPS to 127.0.0.1 if not set
+: ${FORWARDED_ALLOW_IPS:-127.0.0.1}
+
 exec gunicorn nomenklatura.manage:app \
     --bind 0.0.0.0:8080 \
     --workers 4 \
-    --access-logfile -
+    --access-logfile - \
+    --forwarded-allow-ips $FORWARDED_ALLOW_IPS
 # for debugging: add \ to the previous line and
 #   --log-level debug
