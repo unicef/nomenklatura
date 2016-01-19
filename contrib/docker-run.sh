986165daba5ab8368ac8b15843df3c79c0392dc9
Maximilian Richt <maxi@richt.name>
2016-01-19 23:36:34 +0100

    fix FORWARDED_ALLOW_IPS default value

--- contrib/docker-run.sh
+++ contrib/docker-run.sh
@@ -6,7 +6,7 @@ python nomenklatura/manage.py createdb
 # python nomenklatura/manage.py assets build
 
 # sets FORWARDED_ALLOW_IPS to 127.0.0.1 if not set
-: ${FORWARDED_ALLOW_IPS:-127.0.0.1}
+FORWARDED_ALLOW_IPS="${FORWARDED_ALLOW_IPS:-127.0.0.1}"
 
 exec gunicorn nomenklatura.manage:app \
     --bind 0.0.0.0:8080 \
